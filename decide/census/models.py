0ba7fc66949216f3dda6595a9292cf8eab17ddae
davdelcar <davdelcar@alum.us.es>
2023-11-30 12:12:16 +0100

    Conflicto rq1!

--- decide/census/models.py
+++ decide/census/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Census(models.Model):
     voting_id = models.PositiveIntegerField()
-    voter_id = models.PositiveIntegerField()
+    voter_id = models.IntegerField()
+    
 
     class Meta:
         unique_together = (('voting_id', 'voter_id'),)
